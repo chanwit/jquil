5514cc35bf5e652b36c6d18e37cae7f3a677c256
Chanwit Kaewkasi <chanwit@gmail.com>
2010-01-15 16:34:07 +0000

    init

---
+++ grails-app/taglib/JquiTagLib.groovy
@@ -0,0 +1,87 @@
+class JquiTagLib {
+
+    static namespace = "j"
+
+    def using = { attrs, body ->
+        if(!pageScope.variables.containsKey('jquery')) {
+            out << g.javascript(library:"jquery")
+            pageScope.jquery = true
+        }
+        def lib = attrs.remove("library")
+        switch(lib) {
+            case "core": usingCore()
+                         break;
+            case "tree": usingTree()
+                         break;						 
+        }
+    }
+
+    /**
+     Core
+    **/
+    private usingCore() {
+        out << "<script type=\"text/javascript\" src=\"${g.resource(dir:'js',file:'jquery/jquery-ui-1.7.2.min.js')}\"></script>\n"
+        out << "<link rel=\"stylesheet\" href=\"${resource(dir:'css',file:'themes/base/ui.all.css')}\" />\n"
+    }
+    
+    def groupbox = { attrs, body ->
+    	def id = attrs['id']
+    	def caption = attrs.remove('caption')
+    	def image   = attrs.remove('image')
+        out << "<div ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
+        out << "<h3><a href=\"#\">${caption}</a></h3>"
+        out << "<div><p>"
+        out << body()
+        out << "</p></div>"
+        out << "</div>"
+        out << """
+        <script type="text/javascript">
+            jQuery(document).ready(function(){
+                jQuery("#${id}").accordion({
+                	collapsible: true,
+                	animated: false
+                });
+            });
+        </script>"""        
+    }
+
+    /**
+     Tree
+    **/
+    private usingTree() {
+        out << "<script type=\"text/javascript\" src=\"${g.resource(dir:'js',file:'jquery/jquery.treeview.min.js')}\"></script>\n"
+        out << "<link rel=\"stylesheet\" href=\"${resource(dir:'css',file:'jquery.treeview.css')}\" />\n"
+    }
+    
+    def tree = { attrs, body ->
+        def id = attrs['id']
+        def persist = attrs.remove('persist')
+        if(!persist) persist = "location"
+        
+        def collapsed = attrs.remove('collapsed')
+        if(!collapsed) collapsed = "false"
+        
+        def unique = attrs.remove('unique')
+        if(!unique) unique = "false"
+
+        out << "<ul ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
+        out << body()
+        out << "</ul>"
+        out << """
+        <script type="text/javascript">
+            jQuery(document).ready(function(){
+                jQuery("#${id}").treeview({
+                    persist: "${persist}",
+                    collapsed: ${collapsed},
+                    unique: ${unique}
+                });
+            });
+        </script>"""
+    }
+    
+    def treeitem = { attrs, body ->
+    	out << "<li ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
+        out << body()
+        out << "</li>"    	
+    }
+}
