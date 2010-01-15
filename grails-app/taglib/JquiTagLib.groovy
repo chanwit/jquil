0b46df5e0ada5e77e6319c71af405c2ee00e0b73
Chanwit Kaewkasi <chanwit@gmail.com>
2010-01-15 16:57:31 +0000

    refactored j:using

--- grails-app/taglib/JquiTagLib.groovy
+++ grails-app/taglib/JquiTagLib.groovy
@@ -8,26 +8,21 @@ class JquiTagLib {
             pageScope.jquery = true
         }
         def lib = attrs.remove("library")
-        switch(lib) {
-            case "core": usingCore()
-                         break;
-            case "tree": usingTree()
-                         break;						 
-        }
+        "using_${lib}"()
     }
 
     /**
      Core
     **/
-    private usingCore() {
+    private using_core() {
         out << "<script type=\"text/javascript\" src=\"${g.resource(dir:'js',file:'jquery/jquery-ui-1.7.2.min.js')}\"></script>\n"
         out << "<link rel=\"stylesheet\" href=\"${resource(dir:'css',file:'themes/base/ui.all.css')}\" />\n"
     }
-    
+
     def groupbox = { attrs, body ->
-    	def id = attrs['id']
-    	def caption = attrs.remove('caption')
-    	def image   = attrs.remove('image')
+        def id = attrs['id']
+        def caption = attrs.remove('caption')
+        def image   = attrs.remove('image')
         out << "<div ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
         out << "<h3><a href=\"#\">${caption}</a></h3>"
         out << "<div><p>"
@@ -38,29 +33,37 @@ class JquiTagLib {
         <script type="text/javascript">
             jQuery(document).ready(function(){
                 jQuery("#${id}").accordion({
-                	collapsible: true,
-                	animated: false
+                    collapsible: true,
+                    animated: false
                 });
             });
-        </script>"""        
+        </script>"""
     }
+
+    /**
+     Layout
+    **/
+    private using_layout() {
+        out << "<script type=\"text/javascript\" src=\"${g.resource(dir:'js',file:'jquery/jquery.layout.min-1.2.0.js')}\"></script>\n"
+    }
+
 
     /**
      Tree
     **/
-    private usingTree() {
+    private using_tree() {
         out << "<script type=\"text/javascript\" src=\"${g.resource(dir:'js',file:'jquery/jquery.treeview.min.js')}\"></script>\n"
         out << "<link rel=\"stylesheet\" href=\"${resource(dir:'css',file:'jquery.treeview.css')}\" />\n"
     }
-    
+
     def tree = { attrs, body ->
         def id = attrs['id']
         def persist = attrs.remove('persist')
         if(!persist) persist = "location"
-        
+
         def collapsed = attrs.remove('collapsed')
         if(!collapsed) collapsed = "false"
-        
+
         def unique = attrs.remove('unique')
         if(!unique) unique = "false"
 
@@ -78,10 +81,10 @@ class JquiTagLib {
             });
         </script>"""
     }
-    
+
     def treeitem = { attrs, body ->
-    	out << "<li ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
+        out << "<li ${attrs.collect{ k, v -> "$k=\"$v\""}.join(' ')}>"
         out << body()
-        out << "</li>"    	
+        out << "</li>"
     }
 }
